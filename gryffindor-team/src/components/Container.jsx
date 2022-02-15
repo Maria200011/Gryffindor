c9faaf63dece7cecdd36012dcb938e503853a3e7
Maria200011 <maria.jinbashyan.dwin@gmail.com>
2022-02-15 14:18:21 +0400

    new br

---
+++ gryffindor-team/src/components/Container.jsx
@@ -0,0 +1,9 @@
+import './st.css'
+function Cont() {
+    return (
+        <div className='ccontainer' >
+            <h1>sfgvsieyfudgisyuf</h1>
+        </div>
+    )
+} 
+export default Cont
---
+++ gryffindor-team/src/components/Movies.jsx
@@ -0,0 +1,4 @@
+function Movies(){
+    return <h1>sdf</h1>
+}
+export default Movies
